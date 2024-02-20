7d69b705ae8c777b79766e837d7bf4d5fbc83a6c
Minh Ninh Cong (SM) <minh.ninh.cong@stepmediasoftware.com>
2024-02-20 17:02:14 +0700

    add config data

---
+++ LearningProgramming.Identity/Configurations/RoleConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using LearningProgramming.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LearningProgramming.Identity.Configurations
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.HasData(
+                new List<Role>
+                {
+                    new() {
+                        Id = 1,
+                        Name = "Admin",
+                        Description = "Admin management system",
+                        CreatedAt = DateTime.UtcNow,
+                        CreatedBy = 1
+                    }
+                }
+            );
+        }
+    }
+}
---
+++ LearningProgramming.Identity/Configurations/UserRoleConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using LearningProgramming.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LearningProgramming.Identity.Configurations
+{
+    public class UserRoleConfiguration : IEntityTypeConfiguration<UserRole>
+    {
+        public void Configure(EntityTypeBuilder<UserRole> builder)
+        {
+            builder.HasData(
+                new List<UserRole>
+                {
+                    new() {
+                        RoleId = 1,
+                        UserId = 1,
+                    }
+                }
+            );
+        }
+    }
+}
